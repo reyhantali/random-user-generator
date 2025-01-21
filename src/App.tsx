ff54714271501ce1db6843a5a9baf42ce79925c2
reyhantali <reyhan.tali.rt@gmail.com>
2025-01-21 23:57:45 +0300

    API integration done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,62 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+
+interface UserType {
+  cell: string;
+  email: string;
+  phone: string;
+  name: NameType;
+  location: LocationType;
+  picture: PictureType;
+}
+
+interface NameType {
+  first: string;
+  last: string;
+  title: string;
+}
+
+interface LocationType {
+  city: string;
+}
+
+interface PictureType {
+  medium: string;
+}
 
 function App() {
+  const [user, setUser] = useState<UserType>();
+  const [loading, setLoading] = useState<Boolean>(false);
+
+  const fetchUser = async () => {
+    setLoading(true);
+    const response = await axios.get("https://randomuser.me/api/");
+    setLoading(false);
+    setUser(response.data.results[0]);
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div className="w-full h-screen flex justify-center items-center bg-gray-100 ">
       <div className="w-[600px] flex justify-center bg-white py-8 rounded-xl">
-        <div className="flex items-center flex-col text-center">
-          <img src="https://randomuser.me/api/portraits/women/51.jpg" className="rounded-full w-32 h-32 border-4 border-blue-500" />
-          <span>Cenk Merk</span>
-          <span>cenkmerk@gmail.com</span>
-          <span>İzmir</span>
-          <span>+999999</span>
-        </div>
+        {loading ? (
+          <span>Yükleniyor</span>
+        ) : (
+          <div className="flex items-center flex-col text-center">
+            <img
+              src={user?.picture.medium}
+              className="rounded-full w-32 h-32 border-4 border-blue-500"
+            />
+            <span>{user?.name.first}</span>
+            <span>{user?.email}</span>
+            <span>{user?.location.city}</span>
+            <span>{user?.phone}</span>
+          </div>
+        )}
       </div>
     </div>
   );
