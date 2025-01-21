77a3f260477b3d1d2bcc9d8509a86280f6d4215e
reyhantali <reyhan.tali.rt@gmail.com>
2025-01-21 23:14:52 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="w-full h-screen flex justify-center items-center bg-gray-100 ">
+      <div className="w-[600px] flex justify-center bg-white py-8 rounded-xl">
+        <div className="flex items-center flex-col text-center">
+          <img src="https://randomuser.me/api/portraits/women/51.jpg" className="rounded-full w-32 h-32 border-4 border-blue-500" />
+          <span>Cenk Merk</span>
+          <span>cenkmerk@gmail.com</span>
+          <span>İzmir</span>
+          <span>+999999</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
